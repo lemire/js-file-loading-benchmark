526ef73e381f67e96e07616925980b9c1ab96cd4
Daniel Lemire <dlemire@lemire.me>
2023-09-11 19:20:42 -0400

    Ah.

--- alternative/files.mjs
+++ alternative/files.mjs
@@ -9,3 +9,14 @@ function fn() {
 console.log(fn())
 
 run('readFileSync', fn, 200000, 10)
+
+console.log("UTF-8:")
+
+function utf8fn() {
+  const buf = readFileSync('./README.md', 'UTF-8')
+  return buf.length
+}
+
+console.log(utf8fn())
+
+run('readFileSync', utf8fn, 200000, 10)
--- bench_loadfile.js
+++ bench_loadfile.js
@@ -4,9 +4,16 @@ import { bench, run } from "mitata";
 import { existsSync, createWriteStream, readFileSync, mkdirSync } from "node:fs";
 import path from "node:path";
 let filename = 'README.md'
+console.log("UTF-8 load");
 bench(filename, () => {
   const file_content = readFileSync(filename, "utf-8");
   return file_content.length;
 });
+console.log("Raw load");
+bench(filename, () => {
+  const file_content = readFileSync(filename);
+  return file_content.length;
+});
+
 
 await run();
